fab9bbef7b27c93773f14d806b65e010864a0584
Mahmudur Rahman <formahmudbd@gmail.com>
2021-09-13 07:59:41 +0600

    main part finished

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ loadProducts();
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
@@ -38,7 +38,12 @@ const addToCart = (id, price) => {
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  let converted;
+  if (id == 'price') {
+    converted = parseFloat(element);
+  } else {
+    converted = parseInt(element);
+  }
   return converted;
 };
 
@@ -46,8 +51,9 @@ const getInputValue = (id) => {
 const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
-  const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  let total = (convertedOldPrice + convertPrice);
+  total = parseFloat(total.toFixed(2));
+  document.getElementById(id).innerText = total;
 };
 
 // set innerText function
@@ -77,5 +83,5 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = parseFloat(grandTotal.toFixed(2));
 };
